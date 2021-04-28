c1688f4553c6c7a54bef3568f88e4cefa97309c3
victor yeo <victoryeo1974@gmail.com>
2021-04-28 11:04:01 +0800

    add privateFor to deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 const eKYC = artifacts.require("eKYC");
 
 module.exports = function(deployer) {
-  deployer.deploy(eKYC, true);
+  deployer.deploy(eKYC, 0, {privateFor:
+    // this is public key from tm7 of 7node Quorum samples
+    ["ROAZBWtSacxXQrOe3FGAqJDyJjFePR5ce4TSIzmJ0Bc="]
+  });
 };
